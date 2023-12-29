fd61b0a2de0610b6c4c63ccb75b480a16d58088d
Luis Felipe Sales Bignotto <luisfelipe.bignotto@gmail.com>
2023-12-28 21:31:26 -0300

    Improved exception handlers

--- acme/src/main/java/br/com/acmeairlines/exceptions/ErrorValidationData.java
+++ acme/src/main/java/br/com/acmeairlines/exceptions/ErrorValidationData.java
@@ -1,6 +1,5 @@
 package br.com.acmeairlines.exceptions;
 
-import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,20 +7,14 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
-public class TreatingExceptions {
-
-    @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity treatingError404() {
-        return ResponseEntity.notFound().build();
-    }
-
+public class ErrorValidationData {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity treatingError400(MethodArgumentNotValidException ex) {
         var erros = ex.getFieldErrors();
-        return ResponseEntity.badRequest().body(erros.stream().map(ErrorValidationData::new).toList());
+        return ResponseEntity.badRequest().body(erros.stream().map(ErrorValidationDataRecord::new).toList());
     }
-    private record ErrorValidationData(String field, String msg) {
-        public ErrorValidationData(FieldError error) {
+    private record ErrorValidationDataRecord(String field, String msg) {
+        public ErrorValidationDataRecord(FieldError error) {
             this(error.getField(), error.getDefaultMessage());
         }
     }
