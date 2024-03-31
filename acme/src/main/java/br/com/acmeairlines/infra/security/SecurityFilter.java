291b44fdf189d49072e08881c68884dc6e3b338c
Luis Felipe Sales Bignotto <luisfelipe.bignotto@gmail.com>
2024-03-31 11:14:25 -0300

    improved security filter

--- acme/src/main/java/br/com/acmeairlines/infra/security/SecurityFilter.java
+++ acme/src/main/java/br/com/acmeairlines/infra/security/SecurityFilter.java
@@ -26,15 +26,27 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         var token = this.recoverToken(request);
+
         if(token != null){
-            var login = tokenService.validateToken(token);
+            String login = tokenService.validateToken(token);
+            if (login.equalsIgnoreCase("invalid")) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token inválido");
+                return;
+            }
+
             UserDetails user = userRepository.findByEmail(login);
+            if (user == null) {
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Usuário não encontrado");
+                return;
+            }
 
             var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
             SecurityContextHolder.getContext().setAuthentication(authentication);
         }
+
         filterChain.doFilter(request, response);
     }
+
 
     public String recoverToken(HttpServletRequest request){
         var authHeader = request.getHeader("Authorization");
