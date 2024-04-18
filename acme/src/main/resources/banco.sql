2996b8a44337b8ed28c5453c7c7500ac8ad3e80f
Luis Felipe Sales Bignotto <luisfelipe.bignotto@gmail.com>
2024-04-18 15:15:53 -0300

    improved db

--- acme/src/main/resources/banco.sql
+++ acme/src/main/resources/banco.sql
@@ -6,6 +6,7 @@ CREATE TABLE users (
                        id VARCHAR(36) PRIMARY KEY NOT NULL,
                        name VARCHAR(255) NOT NULL,
                        password VARCHAR(255) NOT NULL,
+                       cpf VARCHAR(14) NOT NULL,
                        email VARCHAR(255) NOT NULL UNIQUE,
                        phone VARCHAR(255),
                        zipcode VARCHAR(255),
@@ -40,3 +41,34 @@ CREATE TABLE baggages (
                           FOREIGN KEY (flight_id) REFERENCES flights (id),
                           FOREIGN KEY (user_id) REFERENCES users (id)
 );
+
+CREATE TABLE deleted_flights (
+                          id VARCHAR(36) PRIMARY KEY NOT NULL,
+                          flight_number VARCHAR(255) NOT NULL,
+                          departure_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                          arrival_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+                          departure_airport VARCHAR(255) NOT NULL,
+                          arrival_airport VARCHAR(255) NOT NULL,
+                          deleted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+SET GLOBAL event_scheduler = ON;
+
+DELIMITER $$
+CREATE PROCEDURE archive_past_flights()
+BEGIN
+    INSERT INTO deleted_flights (id, flight_number, departure_date, arrival_date, departure_airport, arrival_airport, deleted_at)
+    SELECT id, flight_number, departure_date, arrival_date, departure_airport, arrival_airport, CURRENT_TIMESTAMP
+    FROM flights
+    WHERE arrival_date < CURRENT_TIMESTAMP;
+
+    DELETE FROM flights
+    WHERE arrival_date < CURRENT_TIMESTAMP;
+END$$
+DELIMITER ;
+
+CREATE EVENT move_expired_flights_event
+ON SCHEDULE EVERY 1 DAY
+STARTS CURRENT_TIMESTAMP + INTERVAL 1 DAY
+DO
+    CALL archive_past_flights();
